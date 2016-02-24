124fa9454afb32c614cc5a859c06e3eeabf861fa
Manish Champsee <manish@champsee.com>
2016-02-23 16:10:20 -0800

    static version of 2015 BadCamp site

---
+++ sites/default/files/js/js_6I0qDtHf7F6EeVduJfkYbdU6-A12gGjeshnrrMFMY3k.js
@@ -0,0 +1,396 @@
+/**
+ * Attaches the calendar behavior to all required fields
+ */
+(function($) {
+  function makeFocusHandler(e) {
+    if (!$(this).hasClass('date-popup-init')) {
+      var datePopup = e.data;
+      // Explicitely filter the methods we accept.
+      switch (datePopup.func) {
+        case 'datepicker':
+          $(this)
+            .datepicker(datePopup.settings)
+            .addClass('date-popup-init');
+          $(this).click(function(){
+            $(this).focus();
+          });
+          break;
+
+        case 'timeEntry':
+          $(this)
+            .timeEntry(datePopup.settings)
+            .addClass('date-popup-init');
+          $(this).click(function(){
+            $(this).focus();
+          });
+          break;
+
+        case 'timepicker':
+          // Translate the PHP date format into the style the timepicker uses.
+          datePopup.settings.timeFormat = datePopup.settings.timeFormat
+            // 12-hour, leading zero,
+            .replace('h', 'hh')
+            // 12-hour, no leading zero.
+            .replace('g', 'h')
+            // 24-hour, leading zero.
+            .replace('H', 'HH')
+            // 24-hour, no leading zero.
+            .replace('G', 'H')
+            // AM/PM.
+            .replace('A', 'p')
+            // Minutes with leading zero.
+            .replace('i', 'mm')
+            // Seconds with leading zero.
+            .replace('s', 'ss');
+
+          datePopup.settings.startTime = new Date(datePopup.settings.startTime);
+          $(this)
+            .timepicker(datePopup.settings)
+            .addClass('date-popup-init');
+          $(this).click(function(){
+            $(this).focus();
+          });
+          break;
+      }
+    }
+  }
+
+  Drupal.behaviors.date_popup = {
+    attach: function (context) {
+      for (var id in Drupal.settings.datePopup) {
+        $('#'+ id).bind('focus', Drupal.settings.datePopup[id], makeFocusHandler);
+      }
+    }
+  };
+})(jQuery);
+;
+(function($){
+/**
+ * To make a form auto submit, all you have to do is 3 things:
+ *
+ * ctools_add_js('auto-submit');
+ *
+ * On gadgets you want to auto-submit when changed, add the ctools-auto-submit
+ * class. With FAPI, add:
+ * @code
+ *  '#attributes' => array('class' => array('ctools-auto-submit')),
+ * @endcode
+ *
+ * If you want to have auto-submit for every form element,
+ * add the ctools-auto-submit-full-form to the form. With FAPI, add:
+ * @code
+ *   '#attributes' => array('class' => array('ctools-auto-submit-full-form')),
+ * @endcode
+ *
+ * If you want to exclude a field from the ctool-auto-submit-full-form auto submission,
+ * add the class ctools-auto-submit-exclude to the form element. With FAPI, add:
+ * @code
+ *   '#attributes' => array('class' => array('ctools-auto-submit-exclude')),
+ * @endcode
+ *
+ * Finally, you have to identify which button you want clicked for autosubmit.
+ * The behavior of this button will be honored if it's ajaxy or not:
+ * @code
+ *  '#attributes' => array('class' => array('ctools-use-ajax', 'ctools-auto-submit-click')),
+ * @endcode
+ *
+ * Currently only 'select', 'radio', 'checkbox' and 'textfield' types are supported. We probably
+ * could use additional support for HTML5 input types.
+ */
+
+Drupal.behaviors.CToolsAutoSubmit = {
+  attach: function(context) {
+    // 'this' references the form element
+    function triggerSubmit (e) {
+      var $this = $(this);
+      if (!$this.hasClass('ctools-ajaxing')) {
+        $this.find('.ctools-auto-submit-click').click();
+      }
+    }
+
+    // the change event bubbles so we only need to bind it to the outer form
+    $('form.ctools-auto-submit-full-form', context)
+      .add('.ctools-auto-submit', context)
+      .filter('form, select, input:not(:text, :submit)')
+      .once('ctools-auto-submit')
+      .change(function (e) {
+        // don't trigger on text change for full-form
+        if ($(e.target).is(':not(:text, :submit, .ctools-auto-submit-exclude)')) {
+          triggerSubmit.call(e.target.form);
+        }
+      });
+
+    // e.keyCode: key
+    var discardKeyCode = [
+      16, // shift
+      17, // ctrl
+      18, // alt
+      20, // caps lock
+      33, // page up
+      34, // page down
+      35, // end
+      36, // home
+      37, // left arrow
+      38, // up arrow
+      39, // right arrow
+      40, // down arrow
+       9, // tab
+      13, // enter
+      27  // esc
+    ];
+    // Don't wait for change event on textfields
+    $('.ctools-auto-submit-full-form input:text, input:text.ctools-auto-submit', context)
+      .filter(':not(.ctools-auto-submit-exclude)')
+      .once('ctools-auto-submit', function () {
+        // each textinput element has his own timeout
+        var timeoutID = 0;
+        $(this)
+          .bind('keydown keyup', function (e) {
+            if ($.inArray(e.keyCode, discardKeyCode) === -1) {
+              timeoutID && clearTimeout(timeoutID);
+            }
+          })
+          .keyup(function(e) {
+            if ($.inArray(e.keyCode, discardKeyCode) === -1) {
+              timeoutID = setTimeout($.proxy(triggerSubmit, this.form), 500);
+            }
+          })
+          .bind('change', function (e) {
+            if ($.inArray(e.keyCode, discardKeyCode) === -1) {
+              timeoutID = setTimeout($.proxy(triggerSubmit, this.form), 500);
+            }
+          });
+      });
+  }
+}
+})(jQuery);
+;
+(function($) {
+
+  Drupal.form_placeholder = {};
+
+  Drupal.form_placeholder.placeholderIsSupported = function() {
+    // Opera Mini v7 doesn’t support placeholder although its DOM seems to indicate so.
+    var isOperaMini = Object.prototype.toString.call(window.operamini) == '[object OperaMini]';
+    return 'placeholder' in document.createElement('input') && !isOperaMini;
+  };
+
+  Drupal.behaviors.form_placeholder = {
+    attach: function(context, settings) {
+      var include = settings.form_placeholder.include;
+      if (include) {
+        include += ', ';
+      }
+      include += '.form-placeholder-include-children *';
+      include += ', .form-placeholder-include';
+      var exclude = settings.form_placeholder.exclude;
+      if (exclude) {
+        exclude += ', ';
+      }
+      exclude += '.form-placeholder-exclude-children *';
+      exclude += ', .form-placeholder-exclude';
+
+      var required_indicator = settings.form_placeholder.required_indicator;
+
+      $(include, context).not(exclude).each(function() {
+        $textfield = $(this);
+
+        // Check if element is a textfield.
+        if (!$textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
+          return;
+        }
+        // Placeholder is supported.
+        else if (Drupal.form_placeholder.placeholderIsSupported() || settings.form_placeholder.fallback_support) {
+          $form = $textfield.closest('form');
+          $label = $form.find('label[for=' + this.id + ']');
+
+          if (required_indicator === 'append') {
+            $label.find('.form-required').insertAfter($textfield).prepend('&nbsp;');
+          }
+          else if (required_indicator === 'remove') {
+            $label.find('.form-required').remove();
+          }
+          else if (required_indicator === 'text') {
+            $label.find('.form-required').text('(' + Drupal.t('required') + ')');
+          }
+
+          $textfield.attr('placeholder', $.trim($label.text()));
+          $label.hide();
+
+          // Fallback support for older browsers.
+          if (!Drupal.form_placeholder.placeholderIsSupported() && settings.form_placeholder.fallback_support) {
+            $textfield.placeholder();
+          }
+        }
+      });
+    }
+  };
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.googleanalytics = {};
+
+$(document).ready(function() {
+
+  // Attach mousedown, keyup, touchstart events to document only and catch
+  // clicks on all elements.
+  $(document.body).bind("mousedown keyup touchstart", function(event) {
+
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+
+      // Is the clicked URL internal?
+      if (Drupal.googleanalytics.isInternal(this.href)) {
+        // Skip 'click' tracking, if custom tracking events are bound.
+        if ($(this).is('.colorbox')) {
+          // Do nothing here. The custom event will handle all tracking.
+          //console.info("Click on .colorbox item has been detected.");
+        }
+        // Is download tracking activated and the file extension configured for download tracking?
+        else if (Drupal.settings.googleanalytics.trackDownload && Drupal.googleanalytics.isDownload(this.href)) {
+          // Download link clicked.
+          ga("send", "event", "Downloads", Drupal.googleanalytics.getDownloadExtension(this.href).toUpperCase(), Drupal.googleanalytics.getPageUrl(this.href));
+        }
+        else if (Drupal.googleanalytics.isInternalSpecial(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          ga("send", "pageview", { "page": Drupal.googleanalytics.getPageUrl(this.href) });
+        }
+      }
+      else {
+        if (Drupal.settings.googleanalytics.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
+          // Mailto link clicked.
+          ga("send", "event", "Mails", "Click", this.href.substring(7));
+        }
+        else if (Drupal.settings.googleanalytics.trackOutbound && this.href.match(/^\w+:\/\//i)) {
+          if (Drupal.settings.googleanalytics.trackDomainMode != 2 || (Drupal.settings.googleanalytics.trackDomainMode == 2 && !Drupal.googleanalytics.isCrossDomain(this.hostname, Drupal.settings.googleanalytics.trackCrossDomains))) {
+            // External link clicked / No top-level cross domain clicked.
+            ga("send", "event", "Outbound links", "Click", this.href);
+          }
+        }
+      }
+    });
+  });
+
+  // Track hash changes as unique pageviews, if this option has been enabled.
+  if (Drupal.settings.googleanalytics.trackUrlFragments) {
+    window.onhashchange = function() {
+      ga('send', 'pageview', location.pathname + location.search + location.hash);
+    }
+  }
+
+  // Colorbox: This event triggers when the transition has completed and the
+  // newly loaded content has been revealed.
+  $(document).bind("cbox_complete", function () {
+    var href = $.colorbox.element().attr("href");
+    if (href) {
+      ga("send", "pageview", { "page": Drupal.googleanalytics.getPageUrl(href) });
+    }
+  });
+
+});
+
+/**
+ * Check whether the hostname is part of the cross domains or not.
+ *
+ * @param string hostname
+ *   The hostname of the clicked URL.
+ * @param array crossDomains
+ *   All cross domain hostnames as JS array.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isCrossDomain = function (hostname, crossDomains) {
+  /**
+   * jQuery < 1.6.3 bug: $.inArray crushes IE6 and Chrome if second argument is
+   * `null` or `undefined`, http://bugs.jquery.com/ticket/10076,
+   * https://github.com/jquery/jquery/commit/a839af034db2bd934e4d4fa6758a3fed8de74174
+   *
+   * @todo: Remove/Refactor in D8
+   */
+  if (!crossDomains) {
+    return false;
+  }
+  else {
+    return $.inArray(hostname, crossDomains) > -1 ? true : false;
+  }
+};
+
+/**
+ * Check whether this is a download URL or not.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isDownload = function (url) {
+  var isDownload = new RegExp("\\.(" + Drupal.settings.googleanalytics.trackDownloadExtensions + ")([\?#].*)?$", "i");
+  return isDownload.test(url);
+};
+
+/**
+ * Check whether this is an absolute internal URL or not.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isInternal = function (url) {
+  var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+  return isInternal.test(url);
+};
+
+/**
+ * Check whether this is a special URL or not.
+ *
+ * URL types:
+ *  - gotwo.module /go/* links.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isInternalSpecial = function (url) {
+  var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+  return isInternalSpecial.test(url);
+};
+
+/**
+ * Extract the relative internal URL from an absolute internal URL.
+ *
+ * Examples:
+ * - http://mydomain.com/node/1 -> /node/1
+ * - http://example.com/foo/bar -> http://example.com/foo/bar
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return string
+ *   Internal website URL
+ */
+Drupal.googleanalytics.getPageUrl = function (url) {
+  var extractInternalUrl = new RegExp("^(https?):\/\/" + window.location.host, "i");
+  return url.replace(extractInternalUrl, '');
+};
+
+/**
+ * Extract the download file extension from the URL.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return string
+ *   The file extension of the passed url. e.g. "zip", "txt"
+ */
+Drupal.googleanalytics.getDownloadExtension = function (url) {
+  var extractDownloadextension = new RegExp("\\.(" + Drupal.settings.googleanalytics.trackDownloadExtensions + ")([\?#].*)?$", "i");
+  var extension = extractDownloadextension.exec(url);
+  return (extension === null) ? '' : extension[1];
+};
+
+})(jQuery);
+;
---
+++ sites/default/files/js/js_pH2ovcSNlws3AWt08ImMXw4cVBJSzdJV2ikmCwnaW8s.js
@@ -0,0 +1,321 @@
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function($) {
+
+  Drupal.form_placeholder = {};
+
+  Drupal.form_placeholder.placeholderIsSupported = function() {
+    // Opera Mini v7 doesn’t support placeholder although its DOM seems to indicate so.
+    var isOperaMini = Object.prototype.toString.call(window.operamini) == '[object OperaMini]';
+    return 'placeholder' in document.createElement('input') && !isOperaMini;
+  };
+
+  Drupal.behaviors.form_placeholder = {
+    attach: function(context, settings) {
+      var include = settings.form_placeholder.include;
+      if (include) {
+        include += ', ';
+      }
+      include += '.form-placeholder-include-children *';
+      include += ', .form-placeholder-include';
+      var exclude = settings.form_placeholder.exclude;
+      if (exclude) {
+        exclude += ', ';
+      }
+      exclude += '.form-placeholder-exclude-children *';
+      exclude += ', .form-placeholder-exclude';
+
+      var required_indicator = settings.form_placeholder.required_indicator;
+
+      $(include, context).not(exclude).each(function() {
+        $textfield = $(this);
+
+        // Check if element is a textfield.
+        if (!$textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
+          return;
+        }
+        // Placeholder is supported.
+        else if (Drupal.form_placeholder.placeholderIsSupported() || settings.form_placeholder.fallback_support) {
+          $form = $textfield.closest('form');
+          $label = $form.find('label[for=' + this.id + ']');
+
+          if (required_indicator === 'append') {
+            $label.find('.form-required').insertAfter($textfield).prepend('&nbsp;');
+          }
+          else if (required_indicator === 'remove') {
+            $label.find('.form-required').remove();
+          }
+          else if (required_indicator === 'text') {
+            $label.find('.form-required').text('(' + Drupal.t('required') + ')');
+          }
+
+          $textfield.attr('placeholder', $.trim($label.text()));
+          $label.hide();
+
+          // Fallback support for older browsers.
+          if (!Drupal.form_placeholder.placeholderIsSupported() && settings.form_placeholder.fallback_support) {
+            $textfield.placeholder();
+          }
+        }
+      });
+    }
+  };
+
+})(jQuery);
+;
+
+(function($) {
+
+/**
+ * Drupal FieldGroup object.
+ */
+Drupal.FieldGroup = Drupal.FieldGroup || {};
+Drupal.FieldGroup.Effects = Drupal.FieldGroup.Effects || {};
+Drupal.FieldGroup.groupWithfocus = null;
+
+Drupal.FieldGroup.setGroupWithfocus = function(element) {
+  element.css({display: 'block'});
+  Drupal.FieldGroup.groupWithfocus = element;
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processFieldset = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any fieldsets containing required fields
+      $('fieldset.fieldset', context).once('fieldgroup-effects', function(i) {
+        if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+          $('legend span.fieldset-legend', $(this)).eq(0).append(' ').append($('.form-required').eq(0).clone());
+        }
+        if ($('.error', $(this)).length) {
+          $('legend span.fieldset-legend', $(this)).eq(0).addClass('error');
+          Drupal.FieldGroup.setGroupWithfocus($(this));
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processAccordion = {
+  execute: function (context, settings, type) {
+    $('div.field-group-accordion-wrapper', context).once('fieldgroup-effects', function () {
+      var wrapper = $(this);
+
+      wrapper.accordion({
+        autoHeight: false,
+        active: '.field-group-accordion-active',
+        collapsible: true,
+        changestart: function(event, ui) {
+          if ($(this).hasClass('effect-none')) {
+            ui.options.animated = false;
+          }
+          else {
+            ui.options.animated = 'slide';
+          }
+        }
+      });
+
+      if (type == 'form') {
+
+        var $firstErrorItem = false;
+
+        // Add required fields mark to any element containing required fields
+        wrapper.find('div.field-group-accordion-item').each(function(i) {
+
+          if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+            $('h3.ui-accordion-header a').eq(i).append(' ').append($('.form-required').eq(0).clone());
+          }
+          if ($('.error', $(this)).length) {
+            // Save first error item, for focussing it.
+            if (!$firstErrorItem) {
+              $firstErrorItem = $(this).parent().accordion("activate" , i);
+            }
+            $('h3.ui-accordion-header').eq(i).addClass('error');
+          }
+        });
+
+        // Save first error item, for focussing it.
+        if (!$firstErrorItem) {
+          $('.ui-accordion-content-active', $firstErrorItem).css({height: 'auto', width: 'auto', display: 'block'});
+        }
+
+      }
+    });
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processHtabs = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any element containing required fields
+      $('fieldset.horizontal-tabs-pane', context).once('fieldgroup-effects', function(i) {
+        if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+          $(this).data('horizontalTab').link.find('strong:first').after($('.form-required').eq(0).clone()).after(' ');
+        }
+        if ($('.error', $(this)).length) {
+          $(this).data('horizontalTab').link.parent().addClass('error');
+          Drupal.FieldGroup.setGroupWithfocus($(this));
+          $(this).data('horizontalTab').focus();
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processTabs = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any fieldsets containing required fields
+      $('fieldset.vertical-tabs-pane', context).once('fieldgroup-effects', function(i) {
+        if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+          $(this).data('verticalTab').link.find('strong:first').after($('.form-required').eq(0).clone()).after(' ');
+        }
+        if ($('.error', $(this)).length) {
+          $(this).data('verticalTab').link.parent().addClass('error');
+          Drupal.FieldGroup.setGroupWithfocus($(this));
+          $(this).data('verticalTab').focus();
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ *
+ * TODO clean this up meaning check if this is really
+ *      necessary.
+ */
+Drupal.FieldGroup.Effects.processDiv = {
+  execute: function (context, settings, type) {
+
+    $('div.collapsible', context).once('fieldgroup-effects', function() {
+      var $wrapper = $(this);
+
+      // Turn the legend into a clickable link, but retain span.field-group-format-toggler
+      // for CSS positioning.
+
+      var $toggler = $('span.field-group-format-toggler:first', $wrapper);
+      var $link = $('<a class="field-group-format-title" href="#"></a>');
+      $link.prepend($toggler.contents());
+
+      // Add required field markers if needed
+      if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+        $link.append(' ').append($('.form-required').eq(0).clone());
+      }
+
+      $link.appendTo($toggler);
+
+      // .wrapInner() does not retain bound events.
+      $link.click(function () {
+        var wrapper = $wrapper.get(0);
+        // Don't animate multiple times.
+        if (!wrapper.animating) {
+          wrapper.animating = true;
+          var speed = $wrapper.hasClass('speed-fast') ? 300 : 1000;
+          if ($wrapper.hasClass('effect-none') && $wrapper.hasClass('speed-none')) {
+            $('> .field-group-format-wrapper', wrapper).toggle();
+          }
+          else if ($wrapper.hasClass('effect-blind')) {
+            $('> .field-group-format-wrapper', wrapper).toggle('blind', {}, speed);
+          }
+          else {
+            $('> .field-group-format-wrapper', wrapper).toggle(speed);
+          }
+          wrapper.animating = false;
+        }
+        $wrapper.toggleClass('collapsed');
+        return false;
+      });
+
+    });
+  }
+};
+
+/**
+ * Behaviors.
+ */
+Drupal.behaviors.fieldGroup = {
+  attach: function (context, settings) {
+    settings.field_group = settings.field_group || Drupal.settings.field_group;
+    if (settings.field_group == undefined) {
+      return;
+    }
+
+    // Execute all of them.
+    $.each(Drupal.FieldGroup.Effects, function (func) {
+      // We check for a wrapper function in Drupal.field_group as
+      // alternative for dynamic string function calls.
+      var type = func.toLowerCase().replace("process", "");
+      if (settings.field_group[type] != undefined && $.isFunction(this.execute)) {
+        this.execute(context, settings, settings.field_group[type]);
+      }
+    });
+
+    // Fixes css for fieldgroups under vertical tabs.
+    $('.fieldset-wrapper .fieldset > legend').css({display: 'block'});
+    $('.vertical-tabs fieldset.fieldset').addClass('default-fallback');
+
+
+    // Add a new ID to each fieldset.
+    $('.group-wrapper fieldset').each(function() {
+      // Tats bad, but we have to keep the actual id to prevent layouts to break.
+      var fieldgorupID = 'field_group-' + $(this).attr('id') + ' ' + $(this).attr('id');
+      $(this).attr('id', fieldgorupID);
+    })
+    // Set the hash in url to remember last userselection.
+    $('.group-wrapper ul li').each(function() {
+      var fieldGroupNavigationListIndex = $(this).index();
+      $(this).children('a').click(function() {
+        var fieldset = $('.group-wrapper fieldset').get(fieldGroupNavigationListIndex);
+        // Grab the first id, holding the wanted hashurl.
+        var hashUrl = $(fieldset).attr('id').replace(/^field_group-/, '').split(' ')[0];
+        window.location.hash = hashUrl;
+      });
+    });
+  }
+};
+
+})(jQuery);;
